32f6e845e93aefbf13a933701a067978c265a84e
jc-programs <jose@gueto.com>
2024-11-28 13:42:43 +0100

    probada fuerza bruta. Funciona OK

--- brute-force.py
+++ brute-force.py
@@ -4,11 +4,9 @@
 import json
 from download_json import download_json
 
-ids = ["E135023", "E234025", "E214024"]
-
 letras = ["A", "E", "J", "S"]
 anyos = ["024", "025"]
-eventos = range(100,1000)
+eventos = range(0,1000)
 
 dl = download_json()
 dl.set_use_sleep(False)
@@ -18,9 +16,8 @@ for letra in letras:
             id = "%s%03d%s" % (letra, evento, anyo)
             json_text, error = dl.get_json(id)
             if json_text is None:
-                print("\tJSON Error: " + error)
+                print(id + " JSON Error: " + error)
             else:
-                print("\tJSON Ok ")
                 json_object = json.loads(json_text)
-                print("\t" + json_object["status"])
+                print(id + " " + json_object["status"])
 
